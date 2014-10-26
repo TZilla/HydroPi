04406cdebb6c9ada5d2e1acbd436f7a8ee66ae07
TZilla <tlzielas@mtu.edu>
2014-10-25 21:36:34 -0400

    Revert "Revert "Revert "Initial edit"""
    This reverts commit 408959f414aa3b26b2c6a5b606424b78e4830335.

--- Python/ControlModule/FanControl/FanControl.py
+++
@@ -1,3 +0,0 @@
-class FanControl(object):
-	def __init__(self):
-		print('This is a Fan Controller')
--- Python/WebServer/static/js/ctmJs.js
+++
@@ -1,7 +0,0 @@
-$('input.filter').on('keyup', function() {
-    var rex = new RegExp($(this).val(), 'i');
-    $('.searchable tr').hide();
-        $('.searchable tr').filter(function() {
-            return rex.test($(this).text());
-        }).show();
-    });
